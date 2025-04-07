76ffd117b3754d4e0e5db75bcee5b38666654e71
Milagros Grimoldi <mgrimoldi@mail.austral.edu.ar>
2025-04-07 15:52:38 -0300

    agrego ruta entidades

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import RegisterPage from './pages/RegisterPage';
+import EntidadesTabs from './components/EntitiesTabs';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -10,6 +11,13 @@ function App() {
                 <Route path="/" element={<Navigate to="/register" />} />
                 {/* Página de registro */}
                 <Route path="/register" element={<RegisterPage />} />
+
+                <Route path="/" element={<Navigate to="/entidades" />} />
+                {/* Página de entidades */}
+                <Route path="/entidades" element={<EntidadesTabs />} />
+                {/* Página de registro */}
+                <Route path="/register" element={<RegisterPage />} />
+
             </Routes>
         </Router>
     );
