6b62cc02f5e9f7dcd6a2eb534b7dd0140d5ad404
Pilar Silveyra <psilveyra@mail.austral.edu.ar>
2025-04-08 12:25:38 -0300

    Api cambios

--- app/models/servicio.py
+++ app/models/servicio.py
@@ -6,6 +6,5 @@ class Servicio(db.Model):
     id_servicio = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
     descripcion = db.Column(db.String(255))
-
     id_entidad = db.Column(db.Integer, db.ForeignKey('entidad.id_entidad'), nullable=False)
     entidad = db.relationship('Entidad', back_populates='servicios') #back_populates es para relacion bidireccional
