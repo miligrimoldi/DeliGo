fc35ffe0baaae50a4097c47b58168d7efc5815e3
Milagros Grimoldi <mgrimoldi@mail.austral.edu.ar>
2025-04-15 10:24:37 -0300

    h

--- app/models/servicio_categoria.py
+++ app/models/servicio_categoria.py
@@ -6,3 +6,7 @@ class ServicioCategoria(db.Model):
     id_servicio = db.Column(db.Integer, db.ForeignKey('servicio.id_servicio'), primary_key=True)
     id_categoria = db.Column(db.Integer, db.ForeignKey('categoria.id_categoria'), primary_key=True)
 # hola
+
+
+#hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+#gg
