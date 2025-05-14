be1bc531c12f64d76ccb04cc11daa865ef34fd85
Milagros Grimoldi <mgrimoldi@mail.austral.edu.ar>
2025-05-14 20:17:39 -0300

    rutas desperdicio cero admin

---
+++ frontend/src/pages/DesperdicioCero.tsx
@@ -0,0 +1,10 @@
+const DesperdicioCero = () => {
+    return (
+        <div style={{ padding: 20 }}>
+            <h2 style={{ fontFamily: "Poppins" }}>Desperdicio Cero</h2>
+            <p>prossimamenchiiii</p>
+        </div>
+    );
+};
+
+export default DesperdicioCero;
