b0a9c208f1456ab61f1897091a033622157ffa8f
Milagros Grimoldi <mgrimoldi@mail.austral.edu.ar>
2025-05-14 20:22:59 -0300

    modal desperdicio cero admin

---
+++ frontend/src/pages/admin/ModalDesperdicioCero.tsx
@@ -0,0 +1,9 @@
+const ModalDesperdicioCero = () => {
+    return (
+        <div>
+            {/* aca va la logica para el desperd. cero*/}
+        </div>
+    );
+};
+
+export default ModalDesperdicioCero;
